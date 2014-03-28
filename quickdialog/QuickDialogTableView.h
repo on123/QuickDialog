0b653d1f2a073746c3af57ccab81e784222dab22
Oliver Nauroth <info@on123.de>
2014-03-28 23:44:01 +0100

    removed compiler warnings

--- quickdialog/QuickDialogTableView.h
+++ quickdialog/QuickDialogTableView.h
@@ -1,13 +1,13 @@
-//                                
+//
 // Copyright 2011 ESCOZ Inc  - http://escoz.com
-// 
-// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this 
-// file except in compliance with the License. You may obtain a copy of the License at 
-// 
-// http://www.apache.org/licenses/LICENSE-2.0 
-// 
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+// file except in compliance with the License. You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software distributed under
-// the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF 
+// the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 // ANY KIND, either express or implied. See the License for the specific language governing
 // permissions and limitations under the License.
 //
@@ -22,15 +22,7 @@
 @class QRootElement;
 @protocol QuickDialogDelegate;
 
-@interface QuickDialogTableView : UITableView {
-
-
-@private
-    __unsafe_unretained QuickDialogController *_controller;
-    QRootElement *_root;
-    id <UITableViewDataSource> quickDialogDataSource;
-    id <UITableViewDelegate> quickDialogDelegate;
-}
+@interface QuickDialogTableView : UITableView
 
 @property(nonatomic, strong) QRootElement *root;
 
@@ -47,8 +39,8 @@
 
 - (UITableViewCell *)cellForElement:(QElement *)element;
 - (void)reloadCellForElements:(QElement *)element, ... NS_REQUIRES_NIL_TERMINATION;
-
 - (void)reloadRowHeights;
 
 - (void)endEditingOnVisibleCells;
+
 @end
